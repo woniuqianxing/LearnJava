ce7aef879931ca2bfc6b6f524ac7fab1f9f015de
woniuqianxing <13096672190@163.com>
2023-12-25 22:45:22 +0800

    后台获取商品详情及PropertiesUtil配置工具,DateTimeUtil时间处理工具

--- java/mmall/src/main/java/com/mmall/service/IProductService.java
+++ java/mmall/src/main/java/com/mmall/service/IProductService.java
@@ -2,8 +2,10 @@ package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Product;
+import com.mmall.vo.ProductDetailVo;
 
 public interface IProductService {
     ServerResponse saveOrUpdateProduct(Product product);
     ServerResponse<String> setSaleStatus(Integer productId,Integer status);
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
 }
