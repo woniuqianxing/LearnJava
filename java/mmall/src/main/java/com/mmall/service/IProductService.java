dc78f026f71c0b154765cf4d30dfc5ff04b9d542
woniuqianxing <13096672190@163.com>
2023-12-27 21:29:10 +0800

    后台商品列表动态分页功能开发

--- java/mmall/src/main/java/com/mmall/service/IProductService.java
+++ java/mmall/src/main/java/com/mmall/service/IProductService.java
@@ -8,4 +8,5 @@ public interface IProductService {
     ServerResponse saveOrUpdateProduct(Product product);
     ServerResponse<String> setSaleStatus(Integer productId,Integer status);
     ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+    ServerResponse getProductList(int pageNum,int pageSize);
 }
